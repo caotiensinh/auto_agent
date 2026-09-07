a82da941e928271a56e48fa30439e9100ce385d6
Mr.Cao <cdn140493@gmail.com>
2026-09-07 12:07:22 +0900

    chore: bump bootstrap after OpenClaw readiness fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-AUTO_AGENT_BOOTSTRAP_VERSION="0.5.0"
+AUTO_AGENT_BOOTSTRAP_VERSION="0.5.1"
 REPO_RAW="${AUTO_AGENT_REPO_RAW:-https://raw.githubusercontent.com/caotiensinh/auto_agent/main}"
 ROLE="${ROLE:-auto}"
 
